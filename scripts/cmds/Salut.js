798b21f271d86bb9a9eebf9ba14084692e3ef8a0
Azizdamiba <azizdamiba329@gmail.com>
2024-10-11 11:35:28 +0000

    Update Salut.js
    Signed-off-by: Azizdamiba <azizdamiba329@gmail.com>

--- scripts/cmds/Salut.js
+++ scripts/cmds/Salut.js
@@ -2,7 +2,7 @@ module.exports = {
 	config: {
 			name: "salut",
 			version: "1.0",
-			author: "Shibai Otsutsuki",
+			author: "aziz",
 			countDown: 5,
 			role: 0,
 			shortDescription: "sarcasm",
@@ -15,6 +15,6 @@ onChat: async function({
 	message,
 	getLang
 }) {
-	if (event.body && event.body.toLowerCase() == "salut") return message.reply("Salut humain! comment tu vas écrit #shibai pour que je t'ajoute dans mon groupe et n'oublie pas que Shibai Otsutsuki es le meilleur 😉");
+	if (event.body && event.body.toLowerCase() == "salut") return message.reply("salut comment ça va ? ");
 }
 };
