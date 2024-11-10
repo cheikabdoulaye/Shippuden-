69335854960a675a2e1e3409088914ab92fd9096
Azizdamiba <azizdamiba329@gmail.com>
2024-11-10 18:03:37 +0000

    Update Salut.js
    Signed-off-by: Azizdamiba <azizdamiba329@gmail.com>

--- scripts/cmds/Salut.js
+++ scripts/cmds/Salut.js
@@ -15,6 +15,6 @@ onChat: async function({
 	message,
 	getLang
 }) {
-	if (event.body && event.body.toLowerCase() == "salut") return message.reply("salut.. Celui qui veut une bastonade peut aller voir 🍀kelem🍀 ");
+	if (event.body && event.body.toLowerCase() == "salut") return message.reply("salut.. je suis l'intelligence artificielle de boulsa. mon lycée a pour proviseur 🥀SAÏDOU YAMEOGO🥀 n/INFOS 🫱🍀kelem🍀 et 💨DAMI 💨");
 }
 };
